442183f2c7a12008c01083cc1be737088aba99af
殇感美 <939257783@qq.com>
2020-08-20 22:29:32 +0800

    登录页优化

--- src/view/main/login/form.vue
+++ src/view/main/login/form.vue
@@ -4,7 +4,7 @@
       <Input size="large" type="text" v-model="form.username" :placeholder="$t('username')" prefix="ios-person-outline" />
     </FormItem>
     <FormItem prop="password">
-      <Input size="large" type="password" v-model="form.password" :placeholder="$t('password')" prefix="ios-lock-outline" />
+      <Input size="large" type="password" v-model="form.password" :placeholder="$t('password')" prefix="ios-lock-outline" @on-enter="submit" />
     </FormItem>
     <FormItem>
       <Button size="large" type="primary" @click="submit" long>{{ $t('login') }}</Button>
